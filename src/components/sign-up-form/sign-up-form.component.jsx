dcf226e73d6d9e54e06d80c3af1f75ae052c63b1
Kon <konsonoftek@gmail.com>
2022-08-22 11:47:37 -0400

    Styled the auth page

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -95,7 +95,7 @@ const SignUpForm = () => {
           value={confirmPassword}
         />
 
-        <Button type='submit'>Submit</Button>
+        <Button type='submit'>Sign Up</Button>
       </form>
     </div>
   );
